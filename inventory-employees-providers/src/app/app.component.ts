dd06c7c16569499ac74936fb571e7b2153256995
Tomás Caruso <143461944+tomasccc@users.noreply.github.com>
2024-10-06 01:50:00 -0300

    registrar-producto-diseño-masomenos

---
+++ inventory-employees-providers/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,ProductComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'inventory-employees-providers';
+}
