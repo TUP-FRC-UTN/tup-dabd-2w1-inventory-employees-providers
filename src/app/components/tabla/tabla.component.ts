9ed4fceed0ebca5f5a42b651761c60d438a99376
113134 - Zea Martin <113134@tecnicatura.frc.utn.edu.ar>
2024-10-16 16:23:18 -0300

    Agregados cambios

--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -1,9 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  OnInit,
-  OnDestroy
-} from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
 import { Producto } from '../../models/producto';
@@ -14,7 +9,6 @@ import * as XLSX from 'xlsx';
 import { GenerateExcelPdfService } from '../../services/generate-excel-pdf.service';
 import { ToastrService } from 'ngx-toastr';
 import { DataTablesModule } from 'angular-datatables';
-
 
 @Component({
   selector: 'app-tabla',
@@ -27,25 +21,30 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
   private table: any; // Referencia para la instancia de DataTable
   productos: Producto[] = []; // Array para almacenar los productos
   filteredProductos: Producto[] = []; // Array para almacenar productos filtrados
-  
+
   exportButtonsEnabled: boolean = true; // Controla la habilitación de los botones
   private stockMap: Map<string, number> = new Map();
 
-  constructor(private productService: ProductService,private excelPdfService: GenerateExcelPdfService) {}
+  constructor(
+    private productService: ProductService,
+    private excelPdfService: GenerateExcelPdfService
+  ) {}
 
   onStartDateChange(): void {
-    const startDateInput: HTMLInputElement = document.getElementById('startDate') as HTMLInputElement;
-    const endDateInput: HTMLInputElement = document.getElementById('endDate') as HTMLInputElement;
-  
+    const startDateInput: HTMLInputElement = document.getElementById(
+      'startDate'
+    ) as HTMLInputElement;
+    const endDateInput: HTMLInputElement = document.getElementById(
+      'endDate'
+    ) as HTMLInputElement;
+
     const startDate = new Date(startDateInput.value);
     const today = new Date(); // Fecha actual
-    
 
     // Actualiza el valor máximo del campo "Hasta". No puede ser mayor a al fecha actual. Se deben bloquear las fechas futuras
-     // Bloquear fechas futuras en el campo "Hasta"
-  const formattedToday = today.toISOString().split('T')[0]; // Obtener la fecha actual en formato yyyy-MM-dd
-  endDateInput.max = formattedToday; // Establecer el valor máximo como la fecha actual
-
+    // Bloquear fechas futuras en el campo "Hasta"
+    const formattedToday = today.toISOString().split('T')[0]; // Obtener la fecha actual en formato yyyy-MM-dd
+    endDateInput.max = formattedToday; // Establecer el valor máximo como la fecha actual
 
     // Actualiza el valor mínimo del campo "Hasta"
     if (startDate) {
@@ -53,26 +52,29 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
     } else {
       endDateInput.min = ''; // Restablecer si no hay fecha de inicio
     }
-  
+
     this.filterByDate(); // Filtrar los productos
   }
-  
+
   onEndDateChange(): void {
-    const startDateInput: HTMLInputElement = document.getElementById('startDate') as HTMLInputElement;
-    const endDateInput: HTMLInputElement = document.getElementById('endDate') as HTMLInputElement;
-  
+    const startDateInput: HTMLInputElement = document.getElementById(
+      'startDate'
+    ) as HTMLInputElement;
+    const endDateInput: HTMLInputElement = document.getElementById(
+      'endDate'
+    ) as HTMLInputElement;
+
     const endDate = new Date(endDateInput.value);
-    
+
     // Actualiza el valor máximo del campo "Desde"
     if (endDate) {
       startDateInput.max = endDateInput.value; // Deshabilitar fechas posteriores a la fecha de fin
     } else {
       startDateInput.max = ''; // Restablecer si no hay fecha de fin
     }
-  
+
     this.filterByDate(); // Filtrar los productos
   }
-  
 
   ngOnInit(): void {
     this.setInitialDates();
@@ -84,8 +86,12 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
     const thirtyDaysAgo = new Date(today);
     thirtyDaysAgo.setDate(today.getDate() - 30);
 
-    const startDateInput: HTMLInputElement = document.getElementById('startDate') as HTMLInputElement;
-    const endDateInput: HTMLInputElement = document.getElementById('endDate') as HTMLInputElement;
+    const startDateInput: HTMLInputElement = document.getElementById(
+      'startDate'
+    ) as HTMLInputElement;
+    const endDateInput: HTMLInputElement = document.getElementById(
+      'endDate'
+    ) as HTMLInputElement;
 
     startDateInput.value = this.formatDateForInput(thirtyDaysAgo);
     endDateInput.value = this.formatDateForInput(today);
@@ -120,17 +126,17 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
       },
       error: (err) => {
         console.error('Error al cargar productos:', err);
-      }
+      },
     });
   }
 
   calculateRunningStock(productos: Producto[]): Producto[] {
     const stockMap = new Map<string, number>();
-    
-    return productos.map(producto => {
+
+    return productos.map((producto) => {
       const currentStock = stockMap.get(producto.product) || 0;
       let newStock: number;
-      
+
       if (producto.modificationType === 'Aumento') {
         newStock = currentStock + producto.amount;
       } else if (producto.modificationType === 'Disminución') {
@@ -138,13 +144,13 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
       } else {
         newStock = currentStock; // En caso de otro tipo de movimiento
       }
-      
+
       stockMap.set(producto.product, newStock);
-      
+
       return {
         ...producto,
         date: this.formatDate(producto.date),
-        stockAfterModification: newStock
+        stockAfterModification: newStock,
       };
     });
   }
@@ -169,18 +175,18 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
         { data: 'supplier', title: 'Proveedor' }, // Columna de proveedor
         { data: 'amount', title: 'Cantidad' }, // Columna de cantidad
         { data: 'description', title: 'Justificativo' }, // Columna de justificativo
-        { data: 'stockAfterModification', title: 'Stock Después' }
+        { data: 'stockAfterModification', title: 'Stock Después' },
       ],
       pageLength: 10, // Número de registros por página
       lengthChange: false, // Deshabilita el selector de cantidad de registros
       language: {
-        search: "Buscar:",
-        info: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+        search: 'Buscar:',
+        info: 'Mostrando _START_ a _END_ de _TOTAL_ registros',
         paginate: {
-          first: "Primero",
-          last: "Último",
-          next: "Siguiente",
-          previous: "Anterior"
+          first: 'Primero',
+          last: 'Último',
+          next: 'Siguiente',
+          previous: 'Anterior',
         },
       },
     });
@@ -188,25 +194,32 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
 
   // Método para filtrar los productos por fecha
   filterByDate(): void {
-    const startDateInput: HTMLInputElement = document.getElementById('startDate') as HTMLInputElement;
-    const endDateInput: HTMLInputElement = document.getElementById('endDate') as HTMLInputElement;
+    const startDateInput: HTMLInputElement = document.getElementById(
+      'startDate'
+    ) as HTMLInputElement;
+    const endDateInput: HTMLInputElement = document.getElementById(
+      'endDate'
+    ) as HTMLInputElement;
 
-
-    const startDate = startDateInput.value ? new Date(startDateInput.value) : null;
+    const startDate = startDateInput.value
+      ? new Date(startDateInput.value)
+      : null;
     const endDate = endDateInput.value ? new Date(endDateInput.value) : null;
 
     // Validar que las fechas sean válidas
     if (startDate && endDate && startDate > endDate) {
-      alert("La fecha de inicio no puede ser mayor que la fecha de fin.");
+      alert('La fecha de inicio no puede ser mayor que la fecha de fin.');
       startDateInput.value = '';
       endDateInput.value = '';
       return;
     }
 
-    this.filteredProductos = this.productos.filter(producto => {
+    this.filteredProductos = this.productos.filter((producto) => {
       const productDate = new Date(this.formatDateyyyyMMdd(producto.date));
-      return (!startDate || productDate >= startDate) &&
-             (!endDate || productDate <= endDate);
+      return (
+        (!startDate || productDate >= startDate) &&
+        (!endDate || productDate <= endDate)
+      );
     });
 
     // Actualizar el DataTable
@@ -215,10 +228,10 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  formatDateyyyyMMdd(dateString : string): string {
+  formatDateyyyyMMdd(dateString: string): string {
     const [day, month, year] = dateString.split('/');
     return `${year}-${month}-${day}`;
-}
+  }
 
   // Método que genera el PDF
   exportToPdf(): void {
@@ -230,7 +243,7 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
       console.error('Tabla no encontrada');
     }
   }
-  
+
   exportToExcel(): void {
     // Seleccionar el elemento de la tabla por su ID
     const table = document.getElementById('historialTable') as HTMLTableElement;
@@ -240,7 +253,6 @@ export class TablaComponent implements OnInit, AfterViewInit, OnDestroy {
       console.error('Tabla no encontrada');
     }
   }
-  
 
   ngOnDestroy(): void {
     if (this.table) {
