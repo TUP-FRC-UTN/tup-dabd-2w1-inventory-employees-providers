05c062eaed06b4ce33115857b8ca17ac377c967d
matias406042 <142633399+matias406042@users.noreply.github.com>
2024-10-18 16:31:48 -0300

    comboyPost

--- src/app/components/iep-form-post-employees/iep-form-post-employees.component.ts
+++ src/app/components/iep-form-post-employees/iep-form-post-employees.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, importProvidersFrom, Inject, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { EmpPostEmployeeService } from '../../services/emp-post-employee.service';
 import { Ciudad, Provincia } from '../../models/emp-provincia';
 import { Observable } from 'rxjs';
 import { Provider } from '../../models/provider';
 import { Supplier } from '../../models/suppliers';
-
+import { AddressDto, Charge, PostEmployeeDto } from '../../models/emp-post-employee-dto';
+import { post } from 'jquery';
 @Component({
   selector: 'app-iep-form-post-employees',
   standalone: true,
@@ -17,6 +18,9 @@ import { Supplier } from '../../models/suppliers';
 export class IEPFormPostEmployeesComponent implements OnInit {
   constructor(private serviceCombos: EmpPostEmployeeService) {}
 
+  createEmployee$: Observable<any>= new Observable<any>();
+
+  userId?: number
   nombre: string = '';
   apellido: string = '';
   cuil: string = '';
@@ -24,7 +28,7 @@ export class IEPFormPostEmployeesComponent implements OnInit {
   telefono?: number;
   mail: string = '';
   calle: string = '';
-  numeroCalle: string = '';
+  numeroCalle: number = 0;
   piso: number = 0;
   dpto: string = '';
   codigoPostal: string = '';
@@ -33,19 +37,36 @@ export class IEPFormPostEmployeesComponent implements OnInit {
   horaEntrada: string = '';
   startTimeContract?: Date;
 
-  terciorizedEmployee: Boolean = false;
+  lunes:boolean=false;
+  martes:boolean=false;
+  miercoles:boolean=false;
+  jueves:boolean=false;
+  viernes:boolean=false;
+  sabado:boolean=false;
+  domingo:boolean=false;
+
+  success:boolean=false;
+  errorMessage:string="";
+  successMessage:string="";
 
   invalidDate: Boolean = false;
 
   provincias: Provincia[] = [];
 
   suppliers: Supplier[] = [];
+  terciorizedEmployee: Boolean = false;
 
   selectedSupplier?: Supplier;
   localidades:Ciudad[]=[];
+  cargos:Charge[]=[];
+
+  cargoSelected?:Charge
   provinciaSelect? : Provincia;
   localidadSelect?:Ciudad;
-
+  
+  postDto:PostEmployeeDto = new PostEmployeeDto();
+  adressDto:AddressDto =new AddressDto();
+ 
 
   public validateDate() {
     if (this.startTimeContract != null) {
@@ -64,12 +85,7 @@ export class IEPFormPostEmployeesComponent implements OnInit {
   }
 
 
-  loadLocalidades(){
 
-    if(this.provinciaSelect!= null){
-    this.localidades=this.provinciaSelect?.ciudades
-    }
-  }
  
 
 
@@ -77,7 +93,80 @@ export class IEPFormPostEmployeesComponent implements OnInit {
     this.terciorizedEmployee = !this.terciorizedEmployee;
   }
 
-  public onSubmit(form: NgForm) {}
+  public onSubmit(form: NgForm) {
+
+      if(form.valid){
+        if(this.adressDto!=null){
+
+          this.adressDto.street=this.calle
+          this.adressDto.city=this.provinciaSelect?.nombre
+          this.adressDto.locality=this.provinciaSelect?.nombre
+          this.adressDto.postal_code=this.codigoPostal
+          this.adressDto.apartment=this.dpto
+          this.adressDto.floor=this.piso
+          this.adressDto.number_street=this.numeroCalle
+
+          if(this.postDto!=null){
+            this.postDto.name=this.nombre;
+            this.postDto.surname=this.apellido;    
+            this.postDto.documenValue=this.dni
+            this.postDto.cuil=this.cuil;
+            this.postDto.telephoneValue=this.telefono
+            this.postDto.emailValue=this.mail;
+            this.postDto.adressDto=this.adressDto
+            this.postDto.salary=this.salario
+            this.postDto.contractStartTime=this.startTimeContract
+            this.postDto.startTime=this.horaEntrada
+            this.postDto.endTime=this.horaSalida
+            this.postDto.supplierId=this.selectedSupplier?.id
+            this.postDto.mondayWorkday=this.lunes
+            this.postDto.thursdayWorkday=this.martes
+            this.postDto.wednesdayWorkday=this.miercoles
+            this.postDto.thursdayWorkday=this.jueves
+            this.postDto.fridayWorkday=this.viernes
+            this.postDto.saturdayWorkday=this.sabado
+            this.postDto.sundayWorkday=this.domingo
+            this.postDto.charge=this.cargoSelected?.id
+            this.postDto.userId=this.userId
+
+            this.createEmployee$ = this.serviceCombos.createProduct(this.postDto);
+            console.log(this.createEmployee$);
+            this.createEmployee$.subscribe({
+              next: response => {
+                this.success = true;
+                this.successMessage = response.message;
+                console.log("PASO: ", response);
+                form.reset();
+              },
+              //formatear estos errores y mostrar en modal succes y error
+              error: error => {
+                if(error.error.message === '400 Ya existe un producto con ese nombre') {
+                  this.errorMessage = 'Ya existe un producto con ese nombre';
+                }else if( error.error.message === '400 No existe el proveedor ingresado') {
+                  this.errorMessage = 'No existe el proveedor ingresado';
+                }else if( error.error.message === '400 No existe la categoria ingresada') {
+                  this.errorMessage = 'La categoría ingresada no existe';
+                }else if( error.error.message === '400 Estado invalido') {
+                  this.errorMessage = 'El estado del producto ingresado es inválido';
+                }else if( error.error.message === '500 Error al crear el producto') {
+                  this.errorMessage = 'Ha ocurrido un error al crear el producto.';
+                }else{
+                  this.errorMessage = 'Ha ocurrido un error al crear el producto.';
+                }
+                console.error(error);
+                this.success = false;
+                form.reset();
+              },
+              complete: () => {
+                console.log('Petición completada');
+              }
+            });
+
+          }
+        }
+      }
+    }
+       
 
   public loadProvincias(): void {
     this.serviceCombos.getProvinces().subscribe({
@@ -97,19 +186,44 @@ export class IEPFormPostEmployeesComponent implements OnInit {
     });
   }
 
-  public loadCombos() {
-    
+
+  public loadCharges(): void {
+    this.serviceCombos.getCharges().subscribe({
+      next: (c) => {
+        this.cargos = c;
+        console.log("cargoss"+c)
+      },
+    });
   }
+
+
+
+ public loadLocalidades():void{
+
+    if(this.provinciaSelect!= null){
+    this.localidades=this.provinciaSelect?.ciudades
+    }
+  }
+
+  
+
+
+ 
+
+
+
 
   ngOnInit(): void {
    this.loadSupplier();
    this.loadProvincias();
+   this.loadCharges();
    console.log(this.provincias)
   }
 
 
 
 
+  
 
 
 
--- src/app/services/emp-post-employee.service.ts
+++ src/app/services/emp-post-employee.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Provincia } from '../models/emp-provincia';
+import { Charge, PostEmployeeDto } from '../models/emp-post-employee-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,11 @@ export class EmpPostEmployeeService {
   private baseUrlSupplier:string = 'http://localhost:8080/suppliers';
 
   private baseUrlProvinces:string ='https://mocki.io/v1/555e5ed8-37c8-480b-8452-7affe6f6f833';
+
+  private baseUrlCharges = 'http://localhost:8080/charges';
+
+  private UrlEmployeePost= 'http://localhost:8080/employees/post';
+
   constructor(private client:HttpClient) { }
 
   private _refresh$= new Subject<void>();
@@ -27,6 +33,23 @@ export class EmpPostEmployeeService {
   getProvinces():Observable<Provincia[]>{
     return this.client.get<Provincia[]>(this.baseUrlProvinces);
   }
+
+  getCharges():Observable<Charge[]>{
+    return this.client.get<Charge[]>(this.baseUrlCharges+'/all');
+  }
+
+
+
+  createProduct(dto: PostEmployeeDto): Observable<any> {
+    const url = `${this.UrlEmployeePost}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const json = JSON.stringify(dto);
+
+    console.log(json);
+
+    return this.client.post<any>(url, json, { headers });
+  }
+
 
 
 
