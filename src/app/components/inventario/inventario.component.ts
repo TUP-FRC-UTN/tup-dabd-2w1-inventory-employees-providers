713e03e6082897456c0179d1d4455da37d2bd784
Tomás Caruso <143461944+tomasccc@users.noreply.github.com>
2024-10-18 00:40:18 -0300

    inventario-mejor

--- src/app/components/inventario/inventario.component.ts
+++ src/app/components/inventario/inventario.component.ts
@@ -17,6 +17,7 @@ import 'datatables.net-dt';
 import 'datatables.net-bs5';
 import { ProductXDetailDto } from '../../models/product-xdetail-dto';
 import DataTable from 'datatables.net-dt';
+import { Details } from '../../models/details';
 
 @Component({
   selector: 'app-inventario',
@@ -50,13 +51,25 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
   categorias: ProductCategory[] = [];
   estados: String[] = [];
   productos: ProductXDetailDto[] = [];
+  details: Details[] = [];
+
+  amountAvailable: number = 0;
+  amountBorrowed: number = 0;
+  amountBroken: number = 0;
 
   categoria: number = 0;
-  reusable?: boolean;
+  reusable: number =0;
   cantMinima: number = 0;
   cantMaxima: number = 0;
   nombre: string = '';
 
+  requestInProcess: boolean = false;
+
+  myMap: Record<string, number> = {
+    'Disponible': 0,
+    'Prestado': 0,
+    'Roto': 0
+  };
   private table: any;
 
   validoMin: boolean = true;
@@ -64,23 +77,25 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
   mensajeValidacionMin: string = "";
   mensajeValidacionMax: string = "";
 
+  valAmount:boolean = false;
+
   ngOnInit(): void {
+    this.initializeDataTable();
     this.cargarDatos();
     this.cargarProductos();
   }
 
   ngAfterViewInit(): void {
-    this.initializeDataTable();
   }
 
   cargarProductos(){
+    this.requestInProcess = true;
     this.productos$ = this.productoService.getAllProducts();
     this.productos$.subscribe({
       next: (productos) => {
         this.productosALL = productos;
-        this.productosFiltered = productos;
-        console.log('PRODUCTOS', this.productosALL);
         this.aplicarFiltros();
+        this.requestInProcess = false;
       },
       error: (error) => {
         console.error(error);
@@ -96,7 +111,7 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
     this.categorias$.subscribe({
       next: (categorias) => {
         this.categorias = categorias;
-        console.log('CATEGORIAS'+this.categorias);
+        console.log(this.categorias);
       },
       error: (error) => {
         console.error(error);
@@ -121,18 +136,63 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
   }
   
   aplicarFiltros(): void {
+    this.productosFiltered=[];
+    if(this.cantMinima>this.cantMaxima){
+      this.validoMin=false;
+    }else{
+      this.validoMin=true;
+    }
+    if(this.cantMaxima<this.cantMinima){
+      this.validoMax=false;
+    }else{
+      this.validoMax=true;
+    }
+
+    if(!this.reusable){
+      this.reusable = 0;
+    }
+    if(!this.cantMaxima){
+      this.cantMaxima = 0;
+    }
+    if(!this.cantMinima){
+      this.cantMinima = 0;
+    }
+    console.log(this.reusable);
     this.productosFiltered = this.productosALL.filter(producto => {
-      const categoriaCumple = this.categoria === 0 || producto.category.categoryId === this.categoria;
-      const reusableCumple = this.reusable === undefined || producto.reusable === this.reusable;
-      const cantMinimaCumple = this.cantMinima === 0 || producto.detailProducts.length >= this.cantMinima;
-      const cantMaximaCumple = this.cantMaxima === 0 || producto.detailProducts.length <= this.cantMaxima;
       const nombreCumple = this.nombre === '' || producto.name.toLowerCase().includes(this.nombre.toLowerCase());
-
-      return categoriaCumple && reusableCumple && cantMinimaCumple && cantMaximaCumple && nombreCumple;
+      const categoriaCumple = this.categoria == 0 || producto.category.categoryId == this.categoria;
+      if(producto.reusable==true && this.reusable == 1){
+        var reusableCumple = true;
+      }else if(producto.reusable==false && this.reusable == 2){
+        var reusableCumple = true;
+      }else if(this.reusable == 0){
+        var reusableCumple = true;
+      }else{
+        var reusableCumple = false;
+      }
+      const amount=producto.detailProducts.length;
+      const minQuantityWarningCumple = this.cantMinima == 0 || amount >= this.cantMinima;
+      const maxQuantityWarningCumple = this.cantMaxima == 0 || amount <= this.cantMaxima;
+      return nombreCumple && categoriaCumple && reusableCumple && minQuantityWarningCumple && maxQuantityWarningCumple;
     });
-    console.log('PRODUCTOS FILTRADOS', this.productosFiltered);
-
+    this.amountAvailable = this.getCountByategoryAndState(1, 'Disponible');
+    this.amountBorrowed = this.getCountByategoryAndState(1, 'Prestado');
+    this.amountBroken = this.getCountByategoryAndState(1, 'Roto');
     this.updateDataTable();
+  }
+
+  getCountByategoryAndState(categoryId: number, state: string): number {
+    return this.productosFiltered.reduce((count, producto) => {
+      if (producto.category.categoryId === categoryId) {
+        for(let i = 0; i < producto.detailProducts.length; i++){
+          console.log(producto.detailProducts[i].state);
+          if(producto.detailProducts[i].state === state){
+            count++;
+          }
+        }
+      }
+      return count;
+    }, 0);
   }
 
 
@@ -140,6 +200,26 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
     this.table = $('#productsList').DataTable({
       data: this.productosFiltered,
       columns: [
+        {
+          data:'detailProducts', title: 'Fecha de último ingreso',
+          render: (data: any) => {
+            let lastDate;
+            for (let i = 0; i < data.length; i++) {
+              if (data[i].lastUpdatedDatetime) {
+                lastDate = data[i].lastUpdatedDatetime;
+                if (data[i].lastUpdatedDatetime > lastDate) {
+                  lastDate = data[i].lastUpdatedDatetime;
+                }
+              }
+            }
+            if(lastDate){
+              return this.formatDate(lastDate);
+            }else{
+              return "";
+            }
+          },
+
+        },
         { data: 'name', title: 'Nombre' },
         { data: 'category', title: 'Categoria',
           render: (data: any) => {
@@ -159,25 +239,7 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
          {
           data: 'minQuantityWarning', title: 'Cantidad mínima para alerta'
          },
-         {
-          data:'detailProducts', title: 'Fecha de último ingreso',
-          render: (data: any) => {
-            let lastDate;
-            for (let i = 0; i < data.length; i++) {
-              if (data[i].lastUpdatedDatetime) {
-                lastDate = data[i].lastUpdatedDatetime;
-                if (data[i].lastUpdatedDatetime > lastDate) {
-                  lastDate = data[i].lastUpdatedDatetime;
-                }
-              }
-            }
-            if(lastDate){
-              return this.formatDate(lastDate);
-            }else{
-              return "";
-            }
-          },
-        },
+         
         {
           data: null,
           title: 'Acciones',
@@ -201,7 +263,9 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
       lengthChange: false,
       searching: false,
       ordering: true,
-      order:[[5, 'desc']],
+      order:[[0, 'desc']],
+      autoWidth: false,  // Desactivar el ajuste automático de ancho
+      
       language: {
         search: "",
         info: "",
@@ -212,7 +276,17 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
           previous: "Anterior"
         },
       },
+      initComplete: () => {
+        $('#Nombre').on('keyup', (event) => {
+          this.nombre = $(event.currentTarget).val() as string;
+          if (this.table.search() !== this.nombre) {
+            this.table.search(this.nombre).draw();
+        }
+        });  
+      }
     });
+
+    
 
     $('#productsList').on('click', '.botonDetalleCrear', (event) => {
       const id = $(event.currentTarget).data('id');
@@ -223,6 +297,8 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
       const id = $(event.currentTarget).data('id');
       this.irDetalles(id);
     });
+
+  
   }
 
   /* METODO PARA PASAR DE FECHAS "2024-10-17" A FORMATO dd/mm/yyyy*/ 
@@ -239,7 +315,7 @@ export class InventarioComponent implements OnInit, OnDestroy, AfterViewInit {
 
   cleanFilters(): void {
     this.categoria = 0;
-    this.reusable = undefined;
+    this.reusable = 0;
     this.cantMinima = 0;
     this.cantMaxima = 0;
     this.nombre = '';
