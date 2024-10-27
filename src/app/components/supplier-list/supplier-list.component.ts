f06f54e0beea4da02d09ecdcaf656dd4b5c4b2b0
Herrera-Nicolas <114403@tecnicatura.frc.utn.edu.ar>
2024-10-26 22:36:57 -0300

    add datatable in supplier

--- src/app/components/supplier-list/supplier-list.component.ts
+++ src/app/components/supplier-list/supplier-list.component.ts
@@ -22,15 +22,106 @@ export class SupplierListComponent implements AfterViewInit {
   date: any = null;
   autorized: boolean = false;
   selectedSupplierId: number | null = null;
+  tableInitialized = false;
+  dataTableInstance: any;
 
   constructor(private supplierService: SuppliersService, private router: Router) { }
 
   ngAfterViewInit(): void {
+
     // Cargar la lista inicial de proveedores al cargar la vista
     this.supplierService.searchSuppliers(null, null, null, false).subscribe(data => {
       this.suppliers = data;
       this.filteredSuppliers = data; // Inicializa con todos los proveedores
+      this.updateDataTable(data);
     });
+    this.initializeDataTable();
+  }
+
+  ngAfterViewChecked(): void {
+    // Asegurarse de que la tabla solo se inicialice una vez
+    if (!this.tableInitialized && this.suppliers.length > 0) {
+      this.initializeDataTable();
+    }
+  }
+
+  initializeDataTable(): void {
+    $(document).ready(() => {
+      this.dataTableInstance = $('#suppliersTable').DataTable({
+        data: this.filteredSuppliers,
+        columns: [
+          { data: 'name', title: 'Razón social' },
+          {
+            data: 'supplierType',
+            title: 'Tipo proveedor',
+            render: (data: string) => {
+              switch (data) {
+                case 'OTHER':
+                  return 'OTRO';
+                case 'OUTSOURCED_SERVICE':
+                  return 'SERVICIO TERCERIZADO';
+                case 'INVENTORY_SUPPLIER':
+                  return 'PROVEEDOR DE INVENTARIO';
+                default:
+                  return data;
+              }
+            }
+          },
+          { data: 'address', title: 'Dirección' },
+          { data: 'healthInsurance', title: 'Obra Social' },
+          { data: 'description', title: 'Descripción' },
+          { data: 'phoneNumber', title: 'Teléfono' },
+          { data: 'email', title: 'Email' },
+          {
+            data: null,
+            title: 'Acciones',
+            render: (data: any, type: any, row: any) => {
+              return `
+                <button type="button" class="btn btn-primary btn-modificar" data-id="${data.id}">Modificar</button>
+                <button type="button" class="btn btn-danger btn-delete" data-bs-toggle="modal" data-bs-target="#deleteModal" data-id="${data.id}">Eliminar</button>
+              `;
+            }
+          }
+        ],
+        pageLength: 10,
+        lengthChange: false,
+        searching: true,
+        destroy: true,
+        language: {
+          search: "Buscar:",
+          info: "Mostrando _START_ a _END_ de _TOTAL_ proveedores",
+          paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+          },
+          emptyTable: "No hay datos disponibles en la tabla",
+        }
+      });
+  
+
+      $('#suppliersTable tbody').on('click', '.btn-delete', (event) => {
+        const id = $(event.currentTarget).data('id');
+        this.setSupplierToDelete(id);
+      });
+  
+
+      $('#suppliersTable tbody').on('click', '.btn-modificar', (event) => {
+        const id = $(event.currentTarget).data('id');
+        this.updateSupplier(id);
+      });
+  
+      this.tableInitialized = true;
+    });
+  }
+  
+  updateDataTable(newSuppliers: any[]): void {
+    if (this.dataTableInstance) {
+      this.dataTableInstance.clear();
+      this.dataTableInstance.rows.add(newSuppliers);
+      this.dataTableInstance.draw();
+    }
   }
 
   setSupplierToDelete(id: number): void {
@@ -38,6 +129,7 @@ export class SupplierListComponent implements AfterViewInit {
   }
 
   confirmDelete(): void {
+    
     if (this.selectedSupplierId !== null) {
       this.deleteSupplier(this.selectedSupplierId);
       this.selectedSupplierId = null;
@@ -48,7 +140,7 @@ export class SupplierListComponent implements AfterViewInit {
     this.supplierService.searchSuppliers(this.name, this.type, this.date, this.autorized).subscribe(
       data => {
         this.suppliers = data;
-        console.log(data);
+        this.updateDataTable(data);
       }
     );
   }
@@ -67,8 +159,10 @@ export class SupplierListComponent implements AfterViewInit {
     this.supplierService.deleteSupplier(id).subscribe(
       response => {
         //alert('¡Proveedor dado de baja correctamente!');
+        console.log(response);
         // Actualizar la lista de proveedores después de eliminar
         this.suppliers = this.suppliers.filter(supplier => supplier.id !== id);
+        this.updateDataTable(this.suppliers);
       },
       error => {
         alert('Ocurrió un error al intentar dar de baja al proveedor.');
