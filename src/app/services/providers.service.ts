b5de57c9dff1ab15a8dd7776526242b534e7f66f
113134 - Zea Martin <113134@tecnicatura.frc.utn.edu.ar>
2024-10-30 17:21:59 -0300

    Update providers.service.ts

--- src/app/services/providers.service.ts
+++ src/app/services/providers.service.ts
@@ -6,8 +6,8 @@ import { delay, Observable } from 'rxjs';
 })
 export class ProvidersService {
 
-  private readonly EMPLOYEE_BASE_URL: string = 'http://localhost:8081/';
-  private readonly SUPPLIERS_URL: string = `${this.EMPLOYEE_BASE_URL}suppliers`;  
+  private readonly INVENTORY_BASE_URL: string = 'http://localhost:8081/';
+  private readonly SUPPLIERS_URL: string = `${this.INVENTORY_BASE_URL}suppliers`;  
 
   constructor(private client:HttpClient) { }
 
